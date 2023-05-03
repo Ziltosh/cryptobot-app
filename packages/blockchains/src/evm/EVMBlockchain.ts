7f10d061e2a9186fb3d0c918a7ca39faf16a1721
nicolas <zeross30@gmail.com>
2023-05-03 12:57:32 +0200

    test avec user agent

--- packages/blockchains/src/evm/EVMBlockchain.ts
+++ packages/blockchains/src/evm/EVMBlockchain.ts
@@ -22,6 +22,9 @@ export class EVMBlockchain {
 
 		this.instance = axios.create({
 			baseURL: this.api_url,
+			headers: {
+				"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
+			}
 		})
 	}
 
